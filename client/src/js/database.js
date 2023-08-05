28154986c1c4b7a6283b3df4a48ecd415fb86886
sethaphelps <sethaphelps@gmail.com>
2023-08-05 08:38:13 -0700

    deploying to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,17 +14,9 @@ const initdb = async () =>
 
 export const putDb = async (content) => {
   const contactDb = await openDB("jate", 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction("jate", "readwrite");
-
-  // Open up the desired object store.
   const store = tx.objectStore("jate");
-
-  // Use the .add() method on the store and pass in the content.
   const request = store.put({ id: 1, value: content });
-
-  // Get confirmation of the request.
   const result = await request;
   console.log("Your data has been saved to the database", result);
 };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "jate",
+        short_name: "text editor",
+        description: "text editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
