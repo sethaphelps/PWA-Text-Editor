249085427880a56525a21b51fe65107d9090569e
sethaphelps <sethaphelps@gmail.com>
2023-08-05 07:54:29 -0700

    added neccessary files and modified js, webpack, and the package json

---
+++ client/src/js/database.js
@@ -0,0 +1,44 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+export const putDb = async (content) => {
+  const contactDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("Your data has been saved to the database", result);
+};
+
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log(":rocket: - data retrieved from the database", result.value)
+    : console.log(":rocket: - data not found in the database");
+};
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      // Webpack plugin that generates our html file and injects our bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Text Editor",
+      }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Contact Cards",
+        short_name: "Contact",
+        description: "Never forget your contacts!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      // CSS Loaders
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
